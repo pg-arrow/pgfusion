8be9d7ba1084ebe3055881e8f96b7e89f70dd24b
nmbr7 <19748270+nmbr7@users.noreply.github.com>
2026-05-15 00:18:10 +0530

    feat: add go arrow flight sql client example

--- examples/client/go/go.mod
+++ examples/client/go/go.mod
@@ -1,3 +1,23 @@
 module pgfusion/flight-sql-client
 
-go 1.22
+go 1.25.0
+
+require (
+	github.com/apache/arrow-go/v18 v18.6.0
+	google.golang.org/grpc v1.81.1
+)
+
+require (
+	github.com/goccy/go-json v0.10.6 // indirect
+	github.com/google/flatbuffers v25.12.19+incompatible // indirect
+	github.com/klauspost/compress v1.18.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.26 // indirect
+	github.com/zeebo/xxh3 v1.1.0 // indirect
+	golang.org/x/exp v0.0.0-20260112195511-716be5621a96 // indirect
+	golang.org/x/net v0.52.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
--- examples/client/go/main.go
+++ examples/client/go/main.go
@@ -1,3 +1,73 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/apache/arrow-go/v18/arrow/flight/flightsql"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const defaultEndpoint = "localhost:32010"
+const defaultQuery = "SELECT count(*) FROM pgbench_accounts"
+
+func main() {
+	endpoint := defaultEndpoint
+	query := defaultQuery
+	if len(os.Args) > 1 {
+		endpoint = os.Args[1]
+	}
+	if len(os.Args) > 2 {
+		query = os.Args[2]
+	}
+
+	client, err := flightsql.NewClient(endpoint, nil, nil, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create flight sql client: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+
+	info, err := client.Execute(ctx, query)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "execute failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("query: %s\n", query)
+	fmt.Printf("endpoints: %d\n", len(info.Endpoint))
+
+	totalRows := int64(0)
+
+	for i, ep := range info.Endpoint {
+		if ep.Ticket == nil {
+			fmt.Fprintf(os.Stderr, "partition %d: no ticket, skipping\n", i)
+			continue
+		}
+
+		stream, err := client.DoGet(ctx, ep.Ticket)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "do_get failed for partition %d: %v\n", i, err)
+			os.Exit(1)
+		}
+
+		rows := int64(0)
+		for stream.Next() {
+			rec := stream.Record()
+			rows += rec.NumRows()
+		}
+		if err := stream.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "partition %d stream error: %v\n", i, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("partition %d: %d rows\n", i, rows)
+		totalRows += rows
+	}
+
+	fmt.Printf("total: %d rows\n", totalRows)
+}
